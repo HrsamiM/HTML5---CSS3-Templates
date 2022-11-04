bc223287d9e42f3ea0615e358c4c51dfcfa61194
Herson Samir Martinez <93807097+HrsamiM@users.noreply.github.com>
2022-11-03 23:47:19 -0600

    Template for carousel slick.js
    template carousel slick.js

---
+++ Carousel template slick.js/carousel-services-template.php
@@ -0,0 +1,89 @@
+
+<!-- Carousel Services -->
+
+<section>
+
+    <div class="container py-1">
+
+        <div class="row mx-auto">
+
+        <!-- Slider Services -->
+
+            <div id="carrusel" class="autoslider-slick">
+
+                <a href="#" class="left-arrow semi-white-bg rounded-circle shodow-lg"><i class="bi bi-chevron-left slider-arrow"></i></a>
+
+                <a href="#" class="right-arrow semi-white-bg rounded-circle shodow-lg"><i class="bi bi-chevron-right slider-arrow"></i></a>
+
+                <div class="carrusel row mx-auto">
+
+                <div class="services col-lg-4 col-md-3 col-sm-12 shadow pt-3 secundary-color-bg" id="service_0">
+                        <div class="p-2">     
+                            <h5 class="py-3 display-5 text-start"><?php echo esc_html($cs1_titlebox); ?></h5>
+                            <div class="boxsl-content">
+                                <p><?php echo esc_html($cs1_descbox); ?></p>
+                            </div>
+                            <a href="#fiber" class="btn btn-primary btn-sm rounded-pill btn-customsecundary global-transparent" role="button"><i class="fa fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="services col-lg-4 col-md-3 col-sm-12 shadow pt-3 primary-color-bg" id="service_1">
+                        <div class="p-2">
+                            <h5 class="py-3 display-5 text-start"><?php echo esc_html($cs2_titlebox); ?></h5>
+                            <div class="boxsl-content">
+                                <p><?php echo esc_html($cs2_descbox); ?></p>
+                            </div>
+                            <a href="#netw" class="btn btn-primary btn-sm rounded-pill btn-customsecundary global-transparent" role="button"><i class="fa fa-arrow-right"></i></a>  
+                        </div>
+                    </div>
+                    <div class="services col-lg-4 col-md-3 col-sm-12 shadow pt-3 secundary-color-bg" id="service_2">
+                        <div class="p-2">
+                           
+                            <h5 class="py-3 display-5 text-start"><?php echo esc_html($cs3_titlebox); ?></h5>
+                            <div class="boxsl-content">
+                                <p><?php echo esc_html($cs4_descbox); ?></p>
+                            </div>
+                            <a href="#aduiov" class="btn btn-primary btn-sm rounded-pill btn-customsecundary global-transparent" role="button"><i class="fa fa-arrow-right"></i></a>
+                        </div>    
+                    </div>
+                    <div class="services col-lg-4 col-md-3 col-sm-12 shadow pt-3 primary-color-bg" id="service_3">
+                        <div class="p-2">
+                            
+                            <h5 class="py-3 display-5 text-start"><?php echo esc_html($cs4_titlebox); ?></h5>
+                            <div class="boxsl-content">
+                                <p><?php echo esc_html($cs4_descbox); ?></p>
+                            </div>
+                            <a href="#voicedata" class="btn btn-primary btn-sm rounded-pill btn-customsecundary global-transparent" role="button"><i class="fa fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="services col-lg-4 col-md-3 col-sm-12 shadow pt-3 secundary-color-bg" id="service_4">
+                        <div class="p-2">
+                            
+                            <h5 class="py-3 display-5 text-start"><?php echo esc_html($cs5_titlebox); ?></h5>
+                            <div class="boxsl-content">
+                                <p><?php echo esc_html($cs5_descbox); ?></p>
+                            </div>
+                            <a href="#categ" class="btn btn-primary btn-sm rounded-pill btn-customsecundary global-transparent" role="button"><i class="fa fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="services col-lg-4 col-md-3 col-sm-12 shadow pt-3 primary-color-bg" id="service_5">
+                        <div class="p-2">
+                        
+                            <h5 class="py-3 display-5 text-start"><?php echo esc_html($cs6_titlebox); ?></h5>
+                            <div class="boxsl-content">
+                                <p><?php echo esc_html($cs6_descbox); ?></p>
+                            </div>
+                            <a href="#camsecuri" class="btn btn-primary btn-sm rounded-pill btn-customsecundary global-transparent" role="button"><i class="fa fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- Slider -->
+
+        </div>
+
+    </div>
+
+</section>
